c7bdbf47e9539b6f1b3d3505779ef3e379f8f15f
Studio Wromo Marketplace <info.wromo@gmail.com>
2024-05-09 04:32:20 +0200

    Update script.js
    Signed-off-by: Studio Wromo Marketplace <info.wromo@gmail.com>

--- INFO/script.js
+++ INFO/script.js
@@ -1,3 +1,5 @@
+var http = require('http');
+
 function login(form) {
   /*put the users and pass you want*/
   if ( form.user.value=="admin" && form.pass.value=="admin123" || form.user.value=="mod" && form.pass.value=="mod123" || form.user.value=="user" && form.pass.value=="user123") { 
